ae32d27688272b8a7541110554ec9c0e76112e21
jcemelanda <jcemelanda@localhost.localdomain>
2016-04-14 18:59:36 -0300

    start classes structure

--- main.py
+++ main.py
@@ -3,27 +3,25 @@ from sys import exit
 import pygame
 
 
-pygame.init()
-score = 0
-lifes = 4
+class MainGame:
+    def __init__(self):
+        pygame.init()
+        self.score = 0
+        self.lifes = 4
+        self.screen = pygame.display.set_mode((956, 560), 0, 32)
 
-screen = pygame.display.set_mode((956, 560), 0, 32)
+    def run(self):
+        while True:
+            self.handle_events()
+            self.draw()
 
+    def draw(self):
+        pygame.display.update()
 
-def handle_events():
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit()
-
-
-def draw():
-    pygame.display.update()
-
-
-def main():
-    while True:
-        handle_events()
-        draw()
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit()
 
 if __name__ == '__main__':
-    main()
+    MainGame().run()
