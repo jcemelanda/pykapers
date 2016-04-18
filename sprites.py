2fa5222234ada2cfae91a9f8a01e50c37098bcb7
iury <satriani-16@hotmail.com>
2016-04-18 00:13:40 -0300

    pep8

--- sprites.py
+++ sprites.py
@@ -57,20 +57,18 @@ class Player(Sprite):
 
 
 class Ladder(Sprite):
-
-	def __init__(self, position, side='right'):
-		super().__init__()
-		self.image = pygame.image.load('ladder.png').convert_alpha()
-		if side == 'left':
-			self.image = pygame.transform.flip(self.image, True, False)
-		self.rect = self.image.get_rect()
-		self.rect = self.rect.move(position)
+    def __init__(self, position, side='right'):
+        super().__init__()
+        self.image = pygame.image.load('ladder.png').convert_alpha()
+        if side == 'left':
+            self.image = pygame.transform.flip(self.image, True, False)
+        self.rect = self.image.get_rect()
+        self.rect = self.rect.move(position)
 
 
 class Platform(Sprite):
-
-	def __init__(self, position):
-		super().__init__()
-		self.image = pygame.image.load('platform.png').convert()
-		self.rect = self.image.get_rect()
-		self.rect = self.rect.move(position)
+    def __init__(self, position):
+        super().__init__()
+        self.image = pygame.image.load('platform.png').convert()
+        self.rect = self.image.get_rect()
+        self.rect = self.rect.move(position)
